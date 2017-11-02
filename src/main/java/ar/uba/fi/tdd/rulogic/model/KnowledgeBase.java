57ed097d30f2cb53f57793842b69c1becbbb1cdc
Your Name <you@example.com>
2017-11-01 23:17:35 -0300

    first

--- src/main/java/ar/uba/fi/tdd/rulogic/model/KnowledgeBase.java
+++ src/main/java/ar/uba/fi/tdd/rulogic/model/KnowledgeBase.java
@@ -1,9 +1,41 @@
 package ar.uba.fi.tdd.rulogic.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class KnowledgeBase {
 
+	
+	private String database;
+	private List<String> facts;
+	
 	public boolean answer(String query) {
 		return true;
+	}
+	
+	public KnowledgeBase setDatabase(String database) {
+		this.database = database;
+		this.setFacts(parseFacts(database));
+		return this;
+	}
+
+	private List<String> parseFacts(String database) {
+		List<String> facts = Arrays.asList(database.split("."));
+		return facts;
+	}
+
+	public KnowledgeBase setFacts(List<String> facts) {
+		this.facts = facts;
+		return this;
+	}
+	
+	public String getDatabase() {
+		return database;
+	}
+
+	public List<String> getFacts() {
+		return facts;
 	}
 
 }
--- src/test/java/ar/uba/fi/tdd/rulogic/model/KnowledgeBaseTest.java
+++ src/test/java/ar/uba/fi/tdd/rulogic/model/KnowledgeBaseTest.java
@@ -2,6 +2,9 @@ package ar.uba.fi.tdd.rulogic.model;
 
 import static org.mockito.MockitoAnnotations.initMocks;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,19 +12,35 @@ import org.mockito.InjectMocks;
 
 public class KnowledgeBaseTest {
 
+	private static final String DB = "padre(homero,bart).";
+
+	
 	@InjectMocks
 	private KnowledgeBase knowledgeBase;
 
 	@Before
 	public void setUp() throws Exception {
 		initMocks(this);
+		knowledgeBase.setDatabase(DB);
 	}
 
 	@Test
 	public void test() {
-
 		Assert.assertTrue(this.knowledgeBase.answer("varon (javier)."));
-
 	}
+	
+	@Test
+	public void myDataBaseLooksGreat() {
+		Assert.assertTrue(this.knowledgeBase.getDatabase().equals(DB));
+	} 
+	
+	
+	@Test
+	public void myFactsLooksGreatToo() {
+		List<String> facts = new ArrayList<String>();
+		facts.add("padre(homero,bart)");
+		Assert.assertTrue(this.knowledgeBase.getFacts().equals(facts));
+	}
+	
 
 }
